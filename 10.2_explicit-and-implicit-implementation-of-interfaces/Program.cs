49006cda99608f469f8445211171a814b55c0597
Dmitriy Lazarenko <fondango@inbox.ru>
2022-05-20 20:45:29 +0300

    10.2 done

---
+++ 10.2_explicit-and-implicit-implementation-of-interfaces/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задание 10.2.2
+//Используя теоретический материал из данного юнита, постарайтесь самостоятельно реализовать явную реализацию следующего интерфейса:
+
+Writer writer = new Writer();
+((IWriter)writer).Write();
+
+//Задание 10.2.3
+//Реализуйте неявно следующий интерфейс:
+
+Worker worker = new Worker();
+worker.Build();
+
+//Задание 10.2.4
+//Реализуйте явно следующий интерфейс и вызовите его метод в классе Program.
+
+((IWorker)worker).Build();
+
+public interface IWriter
+{
+    void Write();
+}
+public class Writer : IWriter
+{
+    public void Write()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+public class Worker : IWorker
+{
+    public void Build()
+    {
+
+    }
+}
+public interface IWorker
+{
+    public void Build();
+}
