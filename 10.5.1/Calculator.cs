58bcc190678f5c081f392a860f17afdcea21ffce
Dmitriy Lazarenko <fondango@inbox.ru>
2022-05-23 20:46:01 +0300

    10.5.1 done

---
+++ 10.5.1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._5._1
+{
+    public class Calculator : IAddition
+    {
+        ILogger logger { get; }
+        public Calculator(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Addition(int a, int b)
+        {
+            logger.Event("Посчитан результат");
+            Console.WriteLine("Сумма чисел: {0}", a+b);
+        }
+    }
+}
---
+++ 10.5.1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задание 1
+// Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел.
+// Определите интерфейс для функции сложения числа и реализуйте его.
+// Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.
+
+// Задание 2
+// Реализуйте механизм внедрения зависимостей: добавьте в мини-калькулятор логгер, используя
+// материал из скринкаста юнита 10.1.
+// Дополнительно: текст ошибки, выводимый в логгере, окрасьте в красный цвет, а текст события — в синий цвет.
+using _10._5._1;
+
+Logger logger = new Logger();
+Calculator calc = new Calculator(logger);
+while (true)
+{
+    try
+    {
+        Console.Write("Введите 1-ое слагаемое: ");
+        var a = Console.ReadLine();
+        Console.Write("Введите 2-ое слагаемое: ");
+        var b = Console.ReadLine();
+        calc.Addition(Convert.ToInt32(a), Convert.ToInt32(b));
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex.Message);
+    }
+}
+
+public interface IAddition
+{
+    void Addition(int a, int b);
+}
+public interface ILogger
+{
+    void Event (string message);
+    void Error (string message);
+}
+public class Logger : ILogger
+{
+    public void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public void Event(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Произошла ошибка: {0}",message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
